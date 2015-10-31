08a37849c599c0f544c6bb1dfbd49cad2df41202
Jiří Mauritz <jirmauritz@gmail.com>
2015-10-31 11:05:26 +0100

    Initial commit: dataset loaded, Java app created

---
+++ src/main/java/cz/muni/fi/walkauth/Main.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.walkauth;
+
+/**
+ * 
+ * 
+ * @author Jiri Mauritz : jirmauritz at gmail dot com
+ */
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+}
