8651f22ba1d58235344612feabeb299b92a0c0f6
Tom Effenberger <tomas.effenberger@gmail.com>
2015-11-03 10:20:22 +0100

    .gitignore update

--- src/main/java/cz/muni/fi/walkauth/Main.java
+++ src/main/java/cz/muni/fi/walkauth/Main.java
@@ -11,6 +11,9 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
+		double[][] values = {{1.2, 2.3}, {0.0, 0.12345}};
+		Matrix m = new Matrix(values);
+		System.out.println(m);
 		
 	}
 	
